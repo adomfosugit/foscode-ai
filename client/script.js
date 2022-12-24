90365c9d3f0501eb227f4a0dc3d5f1a44fc7cb53
Adom <adomfosugit@gmail.com>
2022-12-24 01:30:31 +0000

    first commit

---
+++ client/script.js
@@ -0,0 +1,131 @@
+// get icons from assets
+import bot from './assets/bot.svg';
+import user from './assets/user.svg'
+
+// targeting the html manually
+ const form = document.querySelector('form');// calling by tag form
+ const chatContainer = document.querySelector('#chat_container'); // calling by id chat_container
+
+let loadInterval;
+
+// function that loads our elements
+
+function loader(element){
+  element.textContent = 'Loading';
+
+  loadInterval = setInterval(() => {
+    element.textContent += '.';
+
+    // if loading indicator is max reset to loading
+    if (element.textContent === '....') {
+        element.textContent = ' ';
+    }
+
+  }, 300);
+}
+// implementing AI typing functionality
+function typeText(element, text) {
+  let index = 0
+
+  let interval = setInterval(() => {
+      if (index < text.length) {
+          element.innerHTML += text.charAt(index)
+          index++
+      } else {
+          clearInterval(interval)
+      }
+  }, 30)
+}
+// timestamp between text is 30 milliseconds
+
+// create a unique id  using current time and date 
+
+function generateUniqueId() {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexadecimalString}`;
+}
+//create a chat for AI and one for user
+
+
+function chatStripe(isAi, value, uniqueId) {
+  return (
+      `
+      <div class="wrapper ${isAi && 'ai'}">
+          <div class="chat">
+              <div class="profile">
+                  <img 
+                    src=${isAi ? bot : user} 
+                    alt="${isAi ? 'bot' : 'user'}" 
+                  />
+              </div>
+              <div class="message" id=${uniqueId}>${value}</div>
+          </div>
+      </div>
+  `
+
+  
+  )
+}
+
+
+
+// trigger to get ai response
+const handleSubmit = async (e) => {
+  e.preventDefault()
+
+  const data = new FormData(form)
+
+  // user's chatstripe
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+
+  // to clear the textarea input 
+  form.reset()
+
+  // bot's chatstripe
+  const uniqueId = generateUniqueId()
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+
+  // to focus scroll to the bottom 
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  // specific message div 
+  const messageDiv = document.getElementById(uniqueId)
+
+  // messageDiv.innerHTML = "..."
+  loader(messageDiv)
+
+  const response = await fetch(' http://localhost:5000', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+          prompt: data.get('prompt')
+      })
+  })
+
+  clearInterval(loadInterval)
+  messageDiv.innerHTML = " "
+
+  if (response.ok) {
+      const data = await response.json();
+      const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
+
+      typeText(messageDiv, parsedData)
+  } else {
+      const err = await response.text()
+
+      messageDiv.innerHTML = "Something went wrong"
+      alert(err)
+  }
+}
+// keycode 13 is the enter key
+form.addEventListener('submit', handleSubmit)
+form.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
+      handleSubmit(e)
+  }
+})
